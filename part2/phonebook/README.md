024ad08a7f6dae94efcacca5628ad3ebc4e85b60
janettem <janette.metelinen@gmail.com>
2021-09-01 08:12:19 +0000

    Add part2/phonebook step11-12

--- part2/phonebook/README.md
+++ part2/phonebook/README.md
@@ -5,7 +5,9 @@
 
 [The Phonebook, Step 7-10](https://fullstackopen.com/en/part2/altering_data_in_server#exercises-2-15-2-18)
 
-Create a phonebook application. Implement the addition of a person. If a number is added to an already existing person, the new number will replace the old number. Make it possible for users to delete people from the phonebook. Implement a search field that can be used to filter the list of people by name. Create a server. Fetch the initial people from the server. Save the people that are added to the phonebook to the server.
+[The Phonebook, Step 11-12](https://fullstackopen.com/en/part2/adding_styles_to_react_app#exercises-2-19-2-20)
+
+Create a phonebook application. Implement the addition of a person. If a number is added to an already existing person, the new number will replace the old number. Make it possible for users to delete people from the phonebook. Implement a search field that can be used to filter the list of people by name. Create a server. Fetch the initial people from the server. Save the people that are added to the phonebook to the server. Show a notification that lasts for a few seconds after a person is added or deleted or a number is changed. Show a notification when changing a number fails.
 
 ## Usage
 Open this directory in a terminal.
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,35 +6,51 @@ const App = () => {
   const [filter, setFilter] = useState('')
   const [name, setName] = useState('')
   const [number, setNumber] = useState('')
+  const [message, setMessage] = useState(null)
+
+  const successMessageStyle = {
+    color: 'green',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+
+  const errorMessageStyle = {
+    color: 'red',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+
+  const [messageStyle, setMessageStyle] = useState({})
 
   useEffect(() => {
     personService
       .getAll()
-      .then(initialPersons => {
-        setPersons(initialPersons)
-      })
+      .then(initialPersons => setPersons(initialPersons))
   }, [])
 
   const createPerson = event => {
     event.preventDefault()
 
-    const newPerson = { name, number }
     const person = persons.find(person => name === person.name)
+    const personToCreate = { name, number }
 
     if (person) {
-      if (window.confirm(name + ' is already added to phonebook'
-        + ', replace the old number with a new one?')) {
-        updatePerson(person)
-        setName('')
-        setNumber('')
-      }
+      updatePerson(person)
     } else {
       personService
-        .create(newPerson)
+        .create(personToCreate)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
-          setName('')
-          setNumber('')
+          clearPersonForm()
+          setNotification(`Added ${returnedPerson.name}`, successMessageStyle)
         })
     }
   }
@@ -42,17 +58,24 @@ const App = () => {
   const updatePerson = personToUpdate => {
     const changedPerson = { ...personToUpdate, number }
 
-    personService
-      .update(changedPerson)
-      .then(returnedPerson => {
-        setPersons(persons.map(person =>
-          person.id !== personToUpdate.id ? person : returnedPerson
-        ))
-      })
-      .catch(() => {
-        alert(`${personToUpdate.name} was already deleted from the server`)
-        setPersons(persons.filter(person => person.id !== personToUpdate.id))
-      })
+    if (window.confirm(name + ' is already added to phonebook'
+      + ', replace the old number with a new one?')) {
+      personService
+        .update(changedPerson)
+        .then(returnedPerson => {
+          setPersons(persons.map(person =>
+            person.id !== personToUpdate.id ? person : returnedPerson
+          ))
+          clearPersonForm()
+          setNotification(`Replaced ${personToUpdate.name}'s number`,
+            successMessageStyle)
+        })
+        .catch(() => {
+          setPersons(persons.filter(person => person.id !== personToUpdate.id))
+          setNotification('Information of ' + personToUpdate.name
+            + ' has already been removed from server', errorMessageStyle)
+        })
+    }
   }
 
   const removePerson = personToRemove => {
@@ -61,8 +84,20 @@ const App = () => {
         .remove(personToRemove.id)
         .then(() => {
           setPersons(persons.filter(person => person.id !== personToRemove.id))
+          setNotification(`Deleted ${personToRemove.name}`, successMessageStyle)
         })
     }
+  }
+
+  const clearPersonForm = () => {
+    setName('')
+    setNumber('')
+  }
+
+  const setNotification = (message, messageStyle) => {
+    setMessageStyle(messageStyle)
+    setMessage(message)
+    setTimeout(() => setMessage(null), 5000)
   }
 
   const personsToShow = persons.filter(person =>
@@ -72,6 +107,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} messageStyle={messageStyle} />
       <Filter filter={filter} setFilter={setFilter} />
 
       <h2>Add a new</h2>
@@ -92,10 +128,7 @@ const App = () => {
 const Filter = ({ filter, setFilter }) => (
   <div>
     filter shown with
-    <input
-      value={filter}
-      onChange={event => setFilter(event.target.value)}
-    />
+    <input value={filter} onChange={event => setFilter(event.target.value)} />
   </div>
 )
 
@@ -103,18 +136,12 @@ const PersonForm = ({ createPerson, name, setName, number, setNumber }) => (
   <form onSubmit={createPerson}>
     <div>
       name:
-      <input
-        value={name}
-        onChange={event => setName(event.target.value)}
-      />
+      <input value={name} onChange={event => setName(event.target.value)} />
     </div>
 
     <div>
       number:
-      <input
-        value={number}
-        onChange={event => setNumber(event.target.value)}
-      />
+      <input value={number} onChange={event => setNumber(event.target.value)} />
     </div>
 
     <button type="submit">add</button>
@@ -139,5 +166,17 @@ const Person = ({ person, removePerson }) => (
     <button onClick={removePerson}>delete</button>
   </p>
 )
+
+const Notification = ({ message, messageStyle }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div style={messageStyle}>
+      {message}
+    </div>
+  )
+}
 
 export default App
